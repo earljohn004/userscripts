547465a2e406cad60328cbf7ee4ecf885a297aaa
U-KTDP\abaquitaej <abaquitaej@KTDPPC178.ktdp.local>
2019-10-08 18:20:28 +0800

    initial

---
+++ model/DataHolder.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author abaquitaej
+ */
+public class DataHolder {
+	private int xCoordinate;
+	private int yCoordinate;
+
+	public int getxCoordinate() {
+		return xCoordinate;
+	}
+
+	public void setxCoordinate(int xCoordinate) {
+		this.xCoordinate = xCoordinate;
+	}
+
+	public int getyCoordinate() {
+		return yCoordinate;
+	}
+
+	public void setyCoordinate(int yCoordinate) {
+		this.yCoordinate = yCoordinate;
+	}
+
+
+	
+}
